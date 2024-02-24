3b8a9670a02329b98e66d46278a8a3b3d6d4c5fb
mp2003 <m.p6092003@gmail.com>
2024-02-24 11:26:10 +0530

    configuring OpenAI tokens

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -51,41 +51,8 @@ const Customizer = () => {
     }
   };
 
-  const handleSubmit = async (type) => {
-    if (!prompt) return alert("Please enter a prompt");
-
-    try {
-      setGeneratingImg(true);
-
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
-
-      const data = await response.json();
-
-      handleDecals(type, `data:image/png;base64,${data.photo}`);
-    } catch (error) {
-      alert(error);
-    } finally {
-      setGeneratingImg(false);
-      setActiveEditorTab("");
-    }
-  };
-
-  const handleDecals = (type, result) => {
-    const decalType = DecalTypes[type];
-
-    state[decalType.stateProperty] = result;
-
-    if (!activeFilterTab[decalType.filterTab]) {
-      handleActiveFilterTab(decalType.filterTab);
-    }
+  const handleSubmit = () => {
+    alert(" Sorry Mine OPENAI Credits has exhausted.");
   };
 
   const handleActiveFilterTab = (tabName) => {
